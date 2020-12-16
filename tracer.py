b7ac037761d96463d83b65d4ce7e820a6876db6b
Connor Nelson <Connor@ConnorNelson.com>
2020-12-16 00:54:52 -0700

    Use event loop

--- tracer.py
+++ tracer.py
@@ -63,114 +63,146 @@ class TRACE(ctypes.Structure):
     ]
 
 
+class TraceMachine:
+    def __init__(self, argv):
+        self.argv = argv
+        self.trace = []
+
+    def run(self):
+        trace_pipe_read_path = "/tmp/read"
+        trace_pipe_write_path = "/tmp/write"
+
+        os.mkfifo(trace_pipe_read_path)
+        os.mkfifo(trace_pipe_write_path)
+
+        process = subprocess.Popen(
+            [
+                QEMU_PATH,
+                "-plugin",
+                f"./plugin/libtracer.so,arg={trace_pipe_write_path},arg={trace_pipe_read_path}",
+                *self.argv,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        trace_pipe_read = open(trace_pipe_read_path, "rb")
+        trace_pipe_write = open(trace_pipe_write_path, "wb")
+
+        r_list = [process.stdout, process.stderr, trace_pipe_read]
+        w_list = []
+        x_list = []
+        num_bytes = array.array("i", [0])
+        while r_list:
+            r_available, w_available, x_available = select.select(
+                r_list, w_list, x_list
+            )
+
+            for r in r_available:
+                if r == trace_pipe_read:
+                    data = os.read(r.fileno(), ctypes.sizeof(TRACE_HEADER))
+                    if data:
+                        trace_header = TRACE_HEADER.from_buffer_copy(data)
+                        bb_addr_type = dict(TRACE._fields_)["bb_addrs"]._type_
+                        bb_addrs_size = trace_header.num_addrs * ctypes.sizeof(
+                            bb_addr_type
+                        )
+                        trace_data = os.read(r.fileno(), bb_addrs_size)
+                        trace_addrs = (
+                            trace_header.num_addrs * bb_addr_type
+                        ).from_buffer_copy(trace_data)
+
+                        reason = TRACE_REASON(trace_header.reason)
+
+                        for address in trace_addrs:
+                            self.on_basic_block(address)
+
+                        if reason == TRACE_REASON.trace_full:
+                            pass
+
+                        elif reason == TRACE_REASON.trace_syscall_start:
+                            syscall_nr = trace_header.info.syscall_num
+                            syscall_definition = syscalls.x86_64[syscall_nr]
+                            syscall_args = syscall_definition[2:]
+                            args = list(
+                                trace_header.info.syscall_data.syscall_start_data
+                            )[: len(syscall_args)]
+                            self.on_syscall_start(syscall_nr, *args)
+
+                        elif reason == TRACE_REASON.trace_syscall_end:
+                            syscall_nr = trace_header.info.syscall_num
+                            ret = trace_header.info.syscall_data.syscall_ret
+                            self.on_syscall_end(syscall_nr, ret)
+
+                        os.write(trace_pipe_write.fileno(), b"\x00" * 8)
+
+                elif r == process.stdout:
+                    fcntl.ioctl(r.fileno(), termios.FIONREAD, num_bytes)
+                    data = os.read(r.fileno(), num_bytes[0])
+                    if data:
+                        sys.stdout.buffer.write(data)
+                    # else:
+                    #     sys.stdout.buffer.close()
+
+                elif r == process.stderr:
+                    fcntl.ioctl(r.fileno(), termios.FIONREAD, num_bytes)
+                    data = os.read(r.fileno(), num_bytes[0])
+                    if data:
+                        sys.stderr.buffer.write(data)
+                    # else:
+                    #     sys.stderr.buffer.close()
+
+                if not data:
+                    r_list.remove(r)
+
+    def on_basic_block(self, address):
+        self.trace.append(("bb", address))
+
+    def on_syscall_start(self, syscall_nr, *args):
+        self.current_syscall = syscall_nr
+        self.current_syscall_args = args
+
+    def on_syscall_end(self, syscall_nr, ret):
+        assert syscall_nr == self.current_syscall
+        self.trace.append(("syscall", syscall_nr, self.current_syscall_args, ret))
+        del self.current_syscall
+        del self.current_syscall_args
+
+
+class LogTraceMachine(TraceMachine):
+    def syscall_int_fmt(self, arg):
+        if arg <= 0x100:
+            return str(arg)
+        elif (1 << 32) - 0x100 <= arg < (1 << 32):
+            return str(arg | (-(arg & 0x80000000)))
+        else:
+            return hex(arg)
+
+    def on_syscall_start(self, syscall_nr, *args):
+        super().on_syscall_start(syscall_nr, *args)
+
+        syscall_definition = syscalls.x86_64[syscall_nr]
+        syscall_name = syscall_definition[1]
+
+        if syscall_name.startswith("sys_"):
+            syscall_name = syscall_name[len("sys_") :]
+
+        description_inner = ", ".join(self.syscall_int_fmt(arg) for arg in args)
+        description = f"{syscall_name}({description_inner})"
+        print(description, end=" ")
+
+    def on_syscall_end(self, syscall_nr, ret):
+        super().on_syscall_end(syscall_nr, ret)
+
+        print("=", self.syscall_int_fmt(ret))
+
+
 def main():
-    target = sys.argv[1]
+    machine = LogTraceMachine(sys.argv[1:])
+    machine.run()
 
-    trace_pipe_read_path = "/tmp/read"
-    trace_pipe_write_path = "/tmp/write"
-
-    os.mkfifo(trace_pipe_read_path)
-    os.mkfifo(trace_pipe_write_path)
-
-    process = subprocess.Popen(
-        [
-            QEMU_PATH,
-            "-plugin",
-            f"./plugin/libtracer.so,arg={trace_pipe_write_path},arg={trace_pipe_read_path}",
-            target,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    trace_pipe_read = open(trace_pipe_read_path, "rb")
-    trace_pipe_write = open(trace_pipe_write_path, "wb")
-    basic_block_trace = []
-
-    r_list = [process.stdout, process.stderr, trace_pipe_read]
-    w_list = []
-    x_list = []
-    num_bytes = array.array("i", [0])
-    while r_list:
-        r_available, w_available, x_available = select.select(r_list, w_list, x_list)
-
-        for r in r_available:
-            if r == trace_pipe_read:
-                data = os.read(r.fileno(), ctypes.sizeof(TRACE_HEADER))
-                if data:
-                    trace_header = TRACE_HEADER.from_buffer_copy(data)
-                    bb_addr_type = dict(TRACE._fields_)["bb_addrs"]._type_
-                    bb_addrs_size = trace_header.num_addrs * ctypes.sizeof(bb_addr_type)
-                    trace_data = os.read(r.fileno(), bb_addrs_size)
-                    trace_addrs = (
-                        trace_header.num_addrs * bb_addr_type
-                    ).from_buffer_copy(trace_data)
-
-                    reason = TRACE_REASON(trace_header.reason)
-                    basic_block_trace.extend(trace_addrs)
-
-                    def syscall_int_fmt(arg):
-                        if arg <= 0x100:
-                            return str(arg)
-                        elif (1 << 32) - 0x100 <= arg < (1 << 32):
-                            return str(arg | (-(arg & 0x80000000)))
-                        else:
-                            return hex(arg)
-
-                    if reason == TRACE_REASON.trace_full:
-                        pass
-
-                    elif reason == TRACE_REASON.trace_syscall_start:
-                        syscall_definition = syscalls.x86_64[
-                            trace_header.info.syscall_num
-                        ]
-                        syscall_definition_name = syscall_definition[1]
-                        syscall_definition_args = syscall_definition[2:]
-
-                        if syscall_definition_name.startswith("sys_"):
-                            syscall_definition_name = syscall_definition_name[
-                                len("sys_") :
-                            ]
-
-                        args = trace_header.info.syscall_data.syscall_start_data
-
-                        description_inner = ", ".join(
-                            syscall_int_fmt(args[i])
-                            for i in range(len(syscall_definition_args))
-                        )
-                        description = f"{syscall_definition_name}({description_inner})"
-
-                        print(description, end=" ")
-
-                    elif reason == TRACE_REASON.trace_syscall_end:
-                        print(
-                            "=",
-                            syscall_int_fmt(trace_header.info.syscall_data.syscall_ret),
-                        )
-
-                    os.write(trace_pipe_write.fileno(), b"\x00" * 8)
-
-            elif r == process.stdout:
-                fcntl.ioctl(r.fileno(), termios.FIONREAD, num_bytes)
-                data = os.read(r.fileno(), num_bytes[0])
-                if data:
-                    sys.stdout.buffer.write(data)
-                # else:
-                #     sys.stdout.buffer.close()
-
-            elif r == process.stderr:
-                fcntl.ioctl(r.fileno(), termios.FIONREAD, num_bytes)
-                data = os.read(r.fileno(), num_bytes[0])
-                if data:
-                    sys.stderr.buffer.write(data)
-                # else:
-                #     sys.stderr.buffer.close()
-
-            if not data:
-                r_list.remove(r)
-
-    total_bb_addrs = len(basic_block_trace)
-    unique_bb_addrs = len(set(basic_block_trace))
+    total_bb_addrs = sum(1 for event in machine.trace if event[0] == "bb")
+    unique_bb_addrs = len(set(event[1] for event in machine.trace if event[0] == "bb"))
     print(f"\n\nTraced {total_bb_addrs} basic blocks ({unique_bb_addrs} unique)")
 
 
