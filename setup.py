78d69ad861148e86f60c261148a4b30ea7f95558
Connor Nelson <Connor@ConnorNelson.com>
2021-02-12 13:25:01 -0700

    Release v0.4.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def package_dependencies():
 
 setup(
     name="qtrace",
-    version="0.4.2",
+    version="0.4.3",
     python_requires=">=3.6",
     packages=find_packages(),
     entry_points={
